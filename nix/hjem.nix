55f1f026268b7f84599172a5d46af9b96e277320
schrobingus <brent.monning.jr@gmail.com>
2026-07-25 15:50:27 -0700

    make some parity changes to i3 and sway

--- nix/hjem.nix
+++ nix/hjem.nix
@@ -10,12 +10,16 @@
           "Code/User/settings.json".source     = "${toolboxDirectory}/togohome/.config/Code/User/settings.json";
           "VSCodium/User/settings.json".source = "${toolboxDirectory}/togohome/.config/Code/User/settings.json";
 
+          "i3/config".source   = "${toolboxDirectory}/togohome/.config/i3/config";
+          "sway/config".source = "${toolboxDirectory}/togohome/.config/sway/config";
+
           "dunst".source         = "${toolboxDirectory}/togohome/.config/dunst";
+          "foot".source          = "${toolboxDirectory}/togohome/.config/foot";
           "ghostty".source       = "${toolboxDirectory}/togohome/.config/ghostty";
-          "i3".source            = "${toolboxDirectory}/togohome/.config/i3";
           "i3status".source      = "${toolboxDirectory}/togohome/.config/i3status";
           "picom.conf".source    = "${toolboxDirectory}/togohome/.config/picom.conf";
           "wallpaper.jpg".source = "${toolboxDirectory}/togohome/.config/wallpaper.jpg";
+          "wmstatus.sh".source   = "${toolboxDirectory}/togohome/.config/wmstatus.sh";
         };
         files = {
           ".doom.d/config.el".source   = "${toolboxDirectory}/togohome/.doom.d/config.el";
